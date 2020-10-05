9b3c93f9c9fd854fa5092ff0172404dd7abf2cae
Maximilian Frank <maximilian.frank@ait.ac.at>
2020-10-05 22:08:29 +0200

    Upgrade to 0.13

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-provider "openstack" {
-}
-
 terraform {
   backend "consul" {}
 }
 
 resource "openstack_images_image_v2" "remote_image" {
-  name   = var.image_name
+  name             = var.image_name
   image_source_url = var.image_source
   container_format = var.image_container_format
-  disk_format = var.image_disk_format
+  disk_format      = var.image_disk_format
 }
--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "image_name" {
-	type = string
-	description = "The name to assign to the image"
+  type        = string
+  description = "The name to assign to the image"
 }
 
 variable "image_source" {
-	type = string
-	description = "The remote source of the image"
+  type        = string
+  description = "The remote source of the image"
 }
 
 variable "image_container_format" {
-	type = string
-	description = "The container format to use for the image"
-	default = "bare"
+  type        = string
+  description = "The container format to use for the image"
+  default     = "bare"
 }
 
 variable "image_disk_format" {
-	type = string
-	description = "The disk format to use for the image"
-	default = "qcow2"
+  type        = string
+  description = "The disk format to use for the image"
+  default     = "qcow2"
 }
